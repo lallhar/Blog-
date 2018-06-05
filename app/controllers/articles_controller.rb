86d6aa2c97e083e7467317d7f02fe80d6f53ccc3
lallhar <lfc5timesktt@gmail.com>
2018-06-05 11:20:31 +0530

    added debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,9 @@ class ArticlesController < ApplicationController
 	end
 
 	def create		
+		
 		@article = Article.new(article_params)
+		@article.user = User.first
 		if @article.save
 			#do something
 			flash[:success] = "Artcile was successfully created. Congratulations!"
